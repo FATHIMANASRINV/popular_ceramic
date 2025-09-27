b5b72449b078cfc0c4afd763636f04a4f38a6183
FATHIMANASRIN <fathimanasrinv68@gmail.com>
2025-09-27 10:03:48 +0530

    Add signup form with validation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-        TruncateTablesSeeder::class,
-    ]);        
+                
 // \App\Models\User::factory(10)->create();
     }
 }
